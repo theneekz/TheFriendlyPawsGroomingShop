9375145a4cf2840796c25e3166238b80ae3482fb
Niko Sward <nicholascsward@gmail.com>
2020-06-12 00:47:42 -0500

    more changes

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -7,6 +7,7 @@ export default function Schedule() {
   const [user] = useAuthState(fireApp.auth());
 
   const dbRefObj = fireApp.database().ref();
+
   dbRefObj.on('value', (snap) => console.log(snap.val()));
 
   const services = () => (
@@ -17,17 +18,21 @@ export default function Schedule() {
       <option value="bath">Bath</option>
     </select>
   );
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-  const slots = [
+  let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  let slots = [
+    '7:30 AM - 8:00 AM',
+    '8:00 AM - 8:30 AM',
+    '8:30 AM - 9:00 AM',
+    '9:00 AM - 9:30 AM',
+    '9:30 AM - 10:00 AM',
     '10:00 AM - 10:30 AM',
     '10:30 AM - 11:00 AM',
     '11:00 AM - 11:30 AM',
     '11:30 AM - 12:00 PM',
     '1:30 PM - 2:00 PM',
     '2:00 PM - 2:30 PM',
-    '2:30 PM - 3:00 PM',
   ];
-  const row = (slot) => (
+  let row = (slot) => (
     <tr>
       <td>{slot}</td>
       <td>Available</td>
@@ -37,7 +42,7 @@ export default function Schedule() {
       </td>
     </tr>
   );
-  const dayTable = (day) => (
+  let dayTable = (day) => (
     <div>
       <h1>{day}</h1>
       <table>
